62b15c288c8418772b24398e56f16bcf0caa9eb5
alan <agamez8@miners.utep.edu>
2020-04-03 23:26:16 -0600

    finished AminoAcidLL and AminoAcidTester

--- src/AminoAcidLL.java
+++ src/AminoAcidLL.java
@@ -14,9 +14,21 @@ class AminoAcidLL{
    * pair and increments the codon counter for that codon.
    * NOTE: Does not check for repeats!! */
   AminoAcidLL(String inCodon){
-  
+    aminoAcid = AminoAcidResources.getAminoAcidFromCodon(inCodon);
+    codons = AminoAcidResources.getCodonListForAminoAcid(aminoAcid);
+    counts = new int[codons.length];
+    incrementCodons(inCodon);
+    next = null;
   }
 
+  //increments codons
+  private void incrementCodons(String inCodon) {
+    for (int i = 0; i < codons.length; i++) {
+      if (codons[i].equals(inCodon)) {
+        counts[i]++;
+      }
+    }
+  }
   /********************************************************************************************/
   /* Recursive method that increments the count for a specific codon:
    * If it should be at this node, increments it and stops, 
@@ -24,14 +36,25 @@ class AminoAcidLL{
    * If there is no next node, add a new node to the list that would contain the codon. 
    */
   private void addCodon(String inCodon){
-  //code
+    if(aminoAcid == AminoAcidResources.getAminoAcidFromCodon(codons)) {
+      incrementCodons(codons);
+    }
+
+    if(next != null) {
+      next.addCodon(codons);
+    }
+
+    else {
+      next = AminoAcidLL(codons);
+    }
+
   }
 
 
   /********************************************************************************************/
   /* Shortcut to find the total number of instances of this amino acid */
   private int totalCount(){
-    return 0;
+    return counts[0];
   }
 
   /********************************************************************************************/
@@ -57,7 +80,19 @@ class AminoAcidLL{
   /* Recursive method that finds the differences in **Amino Acid** counts. 
    * the list *must* be sorted to use this method */
   public int aminoAcidCompare(AminoAcidLL inList){
-    return 0;
+    if (isSorted(inList) == true) {
+      return totalDiff(inList);
+    }
+    if(next == null){
+      return inList.totalCount() + aminoAcidCompare(inList);
+    }
+    if(inList.next == null){
+      return totalCount() + next.aminoAcidCompare(inList);
+    }
+    else {
+      return 0;
+    }
+
   }
 
   /********************************************************************************************/
@@ -71,26 +106,51 @@ class AminoAcidLL{
   /********************************************************************************************/
   /* Recursively returns the total list of amino acids in the order that they are in in the linked list. */
   public char[] aminoAcidList(){
-    return new char[]{};
+    if (next == null) {
+      return new char[]{aminoAcid};
+    }
+    else{
+      char[] amino = next.aminoAcidList();
+      char[] list = new char[amino.length + 1];
+
+      for (int i = 0; i < amino.length; i++) {
+        list[0] = amino;
+        list[i + 1] = amino[i];
+      }
+
+      list[0] = aminoAcid;
+      return list;
+
+    }
   }
 
   /********************************************************************************************/
   /* Recursively returns the total counts of amino acids in the order that they are in in the linked list. */
   public int[] aminoAcidCounts(){
-    return new int[]{};
+    return counts;
   }
 
 
   /********************************************************************************************/
   /* recursively determines if a linked list is sorted or not */
   public boolean isSorted(){
-    return false;
+    if(next == null){
+      return true;
+    }
+    if(aminoAcid > next.aminoAcid){
+      return false;
+    }
+    return next.isSorted();
   }
 
 
   /********************************************************************************************/
   /* Static method for generating a linked list from an RNA sequence */
   public static AminoAcidLL createFromRNASequence(String inSequence){
+    AminoAcidLL list = new AminoAcidLL(inSequence.substring(0, 3));
+    inSequence = inSequence;
+    this.next = null;
+    inSequence = inSequence.substring(3);
     return null;
   }
 
@@ -98,6 +158,13 @@ class AminoAcidLL{
   /********************************************************************************************/
   /* sorts a list by amino acid character*/
   public static AminoAcidLL sort(AminoAcidLL inList){
+    AminoAcidLL sortedList = new AminoAcidLL();
+    AminoAcidLL current = inList;
+
+    boolean sorted = false;
+    boolean moved = false;
+
     return null;
   }
+
 }
--- src/AminoAcidResourcesTester.java
+++ src/AminoAcidResourcesTester.java
@@ -1,36 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.Test;
 
-class AminoAcidResourcesTester{
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AminoAcidResourcesTester {
 
   @Test
-  public void allCodons(){
-    char[] rna = {'A','C','U','G'};
-    char[] aa = {'A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W'};
-    for(int i=0; i<4; i++){
-      for(int j=0; j<4; j++){
-        for(int k=0; k<4;k++){
-          String s = new String(new char[]{rna[i],rna[j],rna[k]});
+  public void allCodons() {
+    char[] rna = {'A', 'C', 'U', 'G'};
+    char[] aa = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W'};
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 4; k++) {
+          String s = new String(new char[]{rna[i], rna[j], rna[k]});
           char aaOut = AminoAcidResources.getAminoAcidFromCodon(s);
-          if(aaOut != '*'){
+          if (aaOut != '*') {
             String[] codonList = AminoAcidResources.getCodonListForAminoAcid(aaOut);
             boolean found = false;
-            for(int l=0; l<codonList.length; l++){
+            for (int l = 0; l < codonList.length; l++) {
               found |= (codonList[l].equals(s));
             }
-            if(!found) System.err.println("Codon " + s + " not found, said AA was " + aaOut);
+            if (!found) System.err.println("Codon " + s + " not found, said AA was " + aaOut);
           }
 
           aaOut = AminoAcidResources.getAminoAcidFromCodon(s.toLowerCase());
-          if(aaOut != '*'){
+          if (aaOut != '*') {
             String[] codonList = AminoAcidResources.getCodonListForAminoAcid(aaOut);
             boolean found = false;
-            for(int l=0; l<codonList.length; l++){
+            for (int l = 0; l < codonList.length; l++) {
               found |= (codonList[l].equals(s));
             }
-            if(!found) System.err.println("Codon " + s + " not found, said AA was " + aaOut);
+            if (!found) System.err.println("Codon " + s + " not found, said AA was " + aaOut);
           }
         }
       }
@@ -39,24 +40,104 @@ class AminoAcidResourcesTester{
   }
 
   @Test
-  public void allAAs(){
+  public void allAAs() {
 
-    char[] aa = {'A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W'};
-    for(int i=0; i<aa.length; i++){
+    char[] aa = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W'};
+    for (int i = 0; i < aa.length; i++) {
       String[] codonList = AminoAcidResources.getCodonListForAminoAcid(aa[i]);
-      for(int l=0; l<codonList.length; l++){
-        if(aa[i] != AminoAcidResources.getAminoAcidFromCodon(codonList[l])){
+      for (int l = 0; l < codonList.length; l++) {
+        if (aa[i] != AminoAcidResources.getAminoAcidFromCodon(codonList[l])) {
           System.err.println("AA " + aa[i] + " not found, said codon was " + codonList[l]);
         }
       }
 
       codonList = AminoAcidResources.getCodonListForAminoAcid(Character.toLowerCase(aa[i]));
-      for(int l=0; l<codonList.length; l++){
-        if(aa[i] != AminoAcidResources.getAminoAcidFromCodon(codonList[l])){
+      for (int l = 0; l < codonList.length; l++) {
+        if (aa[i] != AminoAcidResources.getAminoAcidFromCodon(codonList[l])) {
           System.err.println("AA " + aa[i] + " not found, said codon was " + codonList[l]);
         }
       }
     }
+  }
+
+  @Test
+  public void aminoAcidTest1() {
+    char[] expected = {'P', 'T'};
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("GCGGCGGCGUGAAAGGU");
+    assertArrayEquals(expected, test.aminoAcidList());
+  }
+
+  @Test
+  public void aminoAcidCount1() {
+    int[] expected = {3};
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AGGAAGCTGAGGGAGAGG");
+    assertArrayEquals(expected, test.aminoAcidCounts());
+  }
+
+  @Test
+  public void aminoAcidCount2(){
+    int[] expected = {1,2};
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AAAGGCCUUUGAGAUAGAUAG");
+    assertArrayEquals(expected, test.aminoAcidCounts());
+  }
+
+  @Test
+  public void aminoAcidCount3(){
+    int[] expected = {2,3};
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AAAGGGAUAG");
+    assertArrayEquals(expected, test.aminoAcidCounts());
+  }
+
+  @Test
+  public void sequence1(){
+    String current = "XYZ";
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AAGGUUCCAACUGCA");
+    for (int i = 0; i < current.length(); i++) {
+      assertEquals(current.charAt(i), test.aminoAcid);
+      test = test.next;
+    }
+  }
+
+  @Test
+  public void sequence2(){
+    String current = "";
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("GGGUUGCCAAAGGG");
+    for (int i = 0; i < current.length(); i++) {
+      assertEquals(current.charAt(i), test.aminoAcid);
+      test = test.next;
+    }
+  }
+
+  @Test
+  public void sequence3(){
+    AminoAcidLL head = AminoAcidLL.createFromRNASequence("UGUGGUUGCCCAUUUCCCUUACCU");
+    AminoAcidLL sorted = AminoAcidLL.sort(head);
+    int[] expected = {3,2,2,2,2,2};
+    int[] actual = sorted.aminoAcidCounts();
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void sequence4(){
+    AminoAcidLL head = AminoAcidLL.createFromRNASequence("UGUGGUUGCCCAUUUCCCUUACCU");
+    AminoAcidLL sorted = AminoAcidLL.sort(head);
+    int[] expected = {3,1,1,3,1,1};
+    int[] actual = sorted.aminoAcidCounts();
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void sortTest1(){
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AAAGGG");
+    test = AminoAcidLL.sort(test);
+    assertEquals(true, test.isSorted());
+  }
+
+  @Test
+  public void sortTest2(){
+    AminoAcidLL test = AminoAcidLL.createFromRNASequence("AAAGGG");
+    test = AminoAcidLL.sort(test);
+    assertEquals(true, test.isSorted());
   }
 
 }
